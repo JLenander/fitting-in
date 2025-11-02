a9eadb29b652f6d25f00b673306d7fe03aeca531
Jessica <86748529+swannercjj@users.noreply.github.com>
2025-11-02 11:38:01 -0500

    Enable standing and walking freely in cafe #85

--- Assets/Scripts/Level 1/TriggerSeat.cs
+++ Assets/Scripts/Level 1/TriggerSeat.cs
@@ -9,30 +9,54 @@ public class TriggerSeat : MonoBehaviour
     public SceneExitDoor sceneExitDoor;
 
     private bool triggered = false;
+    private bool playerInside = false;
+
+    private Vector3 seatVector = new Vector3(253.3f, 18.1f, 60.1f);
+
+    // only enable collider if we found two hands
+    private int handCount = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
 
     // trigger level start at trigger enter
     private void OnTriggerEnter(Collider other)
     {
-        if (triggered) return;
+        if (playerInside) return;
         if (other != null && other.CompareTag("Hand"))
         {
-            novaLevel1Manager.PlayLevelRoutine();
+            if (!triggered) novaLevel1Manager.PlayLevelRoutine();
             robotMovement.disable = true;
-            SeatRobot(new Vector3(253.3f, 18.1f, 60.1f));
+            SeatRobot();
             triggered = true;
+            playerInside = true;
         }
     }
 
-    void SeatRobot(Vector3 position)
+    private void OnTriggerExit(Collider other)
+    {
+        if (other != null && other.CompareTag("Hand"))
+        {
+            handCount++;
+            Debug.Log(handCount);
+            if (handCount == 2)
+            {
+                playerInside = false;
+                BoxCollider collider = GetComponent<BoxCollider>();
+                collider.enabled = true;
+                handCount = 0;
+            }
+        }
+    }
+
+    public void SeatRobot()
     {
         robotCharController.enabled = false;
-        robot.position = position;
+        robot.position = seatVector;
         robot.rotation = new Quaternion(0, 180, 0, 0);
         GlobalPlayerUIManager.Instance.StopWalkingShake();
         BoxCollider collider = GetComponent<BoxCollider>();
         collider.enabled = false;
+        handCount = 0;
     }
 
     public void StandRobot()
@@ -41,5 +65,11 @@ public class TriggerSeat : MonoBehaviour
         robotMovement.disable = false;
         // enable the exit door collier
         sceneExitDoor.enabled = true;
+        handCount = 0;
+    }
+
+    public bool PlayerInsideSeat()
+    {
+        return playerInside;
     }
 }
