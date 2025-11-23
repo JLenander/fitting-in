2423e6d99455bd36c75d93d32bc2592a9eaa153d
unknown <j.chen1904@gmail.com>
2025-11-22 22:38:39 -0500

    separate left and right hand controls

--- Assets/Scripts/UIScripts/Consoles/LeftArmUIHandler.cs
+++ Assets/Scripts/UIScripts/Consoles/LeftArmUIHandler.cs
@@ -3,34 +3,9 @@ using UnityEngine.UIElements;
 
 public class LeftArmUIHandler : TerminalUIHandler
 {
-    private VisualElement _handElem;
-    
     public static LeftArmUIHandler Instance;
     public void Awake()
     {
         Instance = this;
-    }
-    
-    // Get hand image element from the UI after calling base Start
-    protected override void Start()
-    {
-        base.Start();
-        if (root != null)
-        {
-            _handElem = root.Q<VisualElement>("HandImage");
-        }
-    }
-    
-    // First change the hand image to left hand, then call base ShowUI
-    public override void ShowUI(GameObject player)
-    {
-        if (_handElem != null)
-        {
-            string imagePath = "UI/Terminals/l_hand";
-            var background = new StyleBackground(Resources.Load<Texture2D>(imagePath));
-            _handElem.style.backgroundImage = background;
-        }
-
-        base.ShowUI(player);
     }
 }
--- Assets/Scripts/UIScripts/Consoles/RightArmUIHandler.cs
+++ Assets/Scripts/UIScripts/Consoles/RightArmUIHandler.cs
@@ -3,34 +3,10 @@ using UnityEngine.UIElements;
 
 public class RightArmUIHandler : TerminalUIHandler
 {
-    private VisualElement _handElem;
     
     public static RightArmUIHandler Instance;
     public void Awake()
     {
         Instance = this;
-    }
-    
-    // Get hand image element from the UI after calling base Start
-    protected override void Start()
-    {
-        base.Start();
-        if (root != null)
-        {
-            _handElem = root.Q<VisualElement>("HandImage");
-        }
-    }
-    
-    // First change the hand image to right hand, then call base ShowUI
-    public override void ShowUI(GameObject player)
-    {
-        if (_handElem != null)
-        {
-            string imagePath = "UI/Terminals/r_hand";
-            var background = new StyleBackground(Resources.Load<Texture2D>(imagePath));
-            _handElem.style.backgroundImage = background;
-        }
-
-        base.ShowUI(player);
     }
 }
