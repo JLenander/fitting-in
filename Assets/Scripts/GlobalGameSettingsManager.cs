f193c984e61ac55e3e9b2538b8ac6af3dfe3e21a
Joshua Lenander <52386328+JLenander@users.noreply.github.com>
2025-11-22 21:39:57 -0500

    Add main menu settings and audio slider (#138)
    Fix pause menu max scale
    Add main menu settings panel with fullscreen and master audio slider
    Add master audio slider to pause menu
    Update Main Menu and Level Select to focus on first element by default

---
+++ Assets/Scripts/GlobalGameSettingsManager.cs
@@ -0,0 +1,51 @@
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+
+/// <summary>
+/// Global settings manager to handle persistent game settings like audio settings.
+/// </summary>
+public class GlobalGameSettingsManager : MonoBehaviour
+{
+    public static GlobalGameSettingsManager Instance;
+    
+    private VCA _masterAudio;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+        
+        // Set framerate cap
+        Application.targetFrameRate = 120;
+        
+        _masterAudio = RuntimeManager.GetVCA("vca:/Master");
+    }
+
+    public float GetMasterVolume()
+    {
+        _masterAudio.getVolume(out float volume);
+        return volume;
+    }
+    
+    public void SetMasterVolume(float value)
+    {
+        _masterAudio.setVolume(value);
+    }
+
+    public bool GetFullscreen()
+    {
+        return Screen.fullScreen;
+    }
+    
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
