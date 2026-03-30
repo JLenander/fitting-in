5ec4a037de3abba9bc143b44762868762152adb1
Joshua Lenander <42joshl@gmail.com>
2026-03-30 04:54:39 -0400

    Render extinguisher separately so water stream follows player mouth

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -21,21 +22,36 @@ public class PlayerSetup : MonoBehaviour
         playerInput = GetComponent<PlayerInput>();
         playerId = playerInput.playerIndex + 1;
 
-        int layer = LayerMask.NameToLayer("Player" + playerId);
+        // Layer for things this player's camera does not render (ex. player geometry)
+        int playerIgnoreLayer = LayerMask.NameToLayer("Player" + playerId + "IgnoreRender");
+        if (playerIgnoreLayer == -1) throw new Exception("Player ignore render layer not found for player " + playerId);
+        // Layer for things this player's camera renders (but the specified player's camera does not. Ex. other player only water stream that follows mouth)
+        int playerExclusiveLayer = LayerMask.NameToLayer("Player" + playerId + "OnlyRender");
+        if (playerExclusiveLayer == -1) throw new Exception("Player exclusive render layer not found for player " + playerId);
 
         // Put renderers on correct layer
         foreach (Renderer r in GetComponentsInChildren<Renderer>())
         {
+            // "Ignore" tag designates things that are part of this object that **should** be rendered by this own player's
+            // camera (ie. Should be on a default layer or whatever layer it is set to. ie. ignore the rest of this code)
+            // Tags can't be renamed
             if (r.CompareTag("Ignore"))
                 continue;
 
-            r.gameObject.layer = layer;
+            if (r.CompareTag("PlayerExclusiveRender"))
+                r.gameObject.layer = playerExclusiveLayer;
+            else
+                r.gameObject.layer = playerIgnoreLayer;
         }
 
         // Setup that player’s camera (if it has one)
         Camera cam = GetComponentInChildren<Camera>();
-        if (cam != null)
-            cam.cullingMask &= ~(1 << layer);
+        if (cam == null)
+            throw  new Exception("Camera not found in player " + playerId);
+        // Remove layer for this player's ignored rendering
+        cam.cullingMask &= ~(1 << playerIgnoreLayer);
+        // Add layer for this player's exclusive rendering
+        cam.cullingMask |= (1 << playerExclusiveLayer);
 
         splitscreenUIHandler = FindAnyObjectByType<SplitscreenUIHandler>();
     }
