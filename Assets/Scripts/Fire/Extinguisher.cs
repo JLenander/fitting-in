2779ba02178ba0ab23993a5c589ca9b8a2c44207
Joshua Lenander <52386328+JLenander@users.noreply.github.com>
2026-03-31 01:31:37 -0400

    Add and polish high-poly plorp model (#149)
    Add high poly plorp model.
    Lock player rotation in char select after readying up.
    Fix hole in top of charselect tubes.
    Render extinguisher separately so water stream follows player mouth.

--- Assets/Scripts/Fire/Extinguisher.cs
+++ Assets/Scripts/Fire/Extinguisher.cs
@@ -1,83 +1,58 @@
 using UnityEngine;
 using FMODUnity;
+using UnityEngine.Serialization;
 
 /// <summary>
 /// on active this shoots a ray out to find fires
 /// </summary>
 public class Extinguisher : MonoBehaviour
 {
-    public GameObject waterStream; // the graphic, maybe adjust z scaling based on distance
-    public GameObject waterCylinder;
-    public GameObject endSpray;
+    // Other render is only rendered to other player's camera.
+    public GameObject otherPlayerWaterStream;
+    public GameObject otherPlayerWaterCylinder;
+    public GameObject otherPlayerEndSpray;
+    // Exclusive render is only rendered to this player's camera.
+    [FormerlySerializedAs("waterStream")] public GameObject exclusiveRenderWaterStream; // the graphic, maybe adjust z scaling based on distance
+    [FormerlySerializedAs("waterCylinder")] public GameObject exclusiveRenderWaterCylinder;
+    [FormerlySerializedAs("endSpray")] public GameObject exclusiveRenderEndSpray;
     public float reach = 10f;
     public LayerMask fireLayer;
     private bool active;
-    private bool reset;
     public StudioEventEmitter waterSfx;
 
     void Start()
     {
         // disable at start
-        waterStream.SetActive(false);
-        active = false;
-        reset = true;
-
-        Vector3 scale = waterCylinder.transform.localScale;
-        scale.z = 250;
-        waterCylinder.transform.localScale = scale;
+        ActivateExtinguisher(false);
+        
+        UpdateWaterStream();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (!active) return;
-
-        Ray ray = new Ray(transform.position, transform.forward);
+        
+        Ray fireRay = new Ray(transform.position, transform.forward);
         RaycastHit hit;
-
-        // Debug.DrawRay(ray.origin, ray.direction * reach, Color.cyan);
-
-        if (Physics.Raycast(ray, out hit, reach, fireLayer))
+        
+        if (Physics.Raycast(fireRay, out hit, reach, fireLayer))
         {
             Fire fire = hit.collider.GetComponent<Fire>();
             if (fire != null)
             {
                 fire.ReduceFire();
-
-                // shorten water stream
-                float dist = hit.distance;
-                Vector3 scale = waterCylinder.transform.localScale;
-                scale.z = dist * 50; // model is shrunk for some reason
-                waterCylinder.transform.localScale = scale;
-
-                if (endSpray != null)
-                {
-                    endSpray.transform.position = hit.point + Vector3.down * 0.5f;
-                    endSpray.SetActive(true);
-                }
-
-                reset = false;
             }
         }
-        else
-        {
-            if (!reset)
-            {
-                reset = true;
-                Vector3 scale = waterCylinder.transform.localScale;
-                scale.z = 250;
-                waterCylinder.transform.localScale = scale;
-            }
-
-            if (endSpray != null)
-                endSpray.SetActive(false);
-        }
+        
+        UpdateWaterStream();
     }
 
     public void ActivateExtinguisher(bool state)
     {
         active = state;
-        waterStream.SetActive(state);
+        exclusiveRenderWaterStream.SetActive(state);
+        otherPlayerWaterStream.SetActive(state);
         if (state && !waterSfx.IsPlaying()) 
         {
             Debug.Log($"Water sound start");
@@ -87,6 +62,48 @@ public class Extinguisher : MonoBehaviour
         {
             Debug.Log($"Water sound stop");
             waterSfx.Stop();
+        }
+    }
+
+    /// <summary>
+    /// Update the water streams (exclusive render and other player render) visually
+    /// based on raycasts (one for each render type)
+    /// </summary>
+    private void UpdateWaterStream()
+    {
+       UpdateWaterStreamHelper(exclusiveRenderWaterStream, exclusiveRenderWaterCylinder, exclusiveRenderEndSpray);
+       UpdateWaterStreamHelper(otherPlayerWaterStream, otherPlayerWaterCylinder, otherPlayerEndSpray);
+    }
+
+    private void UpdateWaterStreamHelper(GameObject waterStream, GameObject waterCylinder, GameObject endSpray)
+    {
+        var waterStreamContainerTransform = waterStream.transform.parent;
+        
+        // Rendering for the water stream
+        Ray waterRay = new Ray(waterStreamContainerTransform.position, waterStreamContainerTransform.forward);
+        RaycastHit hit;
+        
+        if (Physics.Raycast(waterRay, out hit, reach))
+        {
+            // shorten water stream
+            float dist = hit.distance;
+            Vector3 scale = waterCylinder.transform.localScale;
+            scale.z = dist * 50; // model is shrunk for some reason
+
+            if (endSpray != null)
+            {
+                endSpray.transform.position = hit.point + Vector3.down * 0.5f;
+                endSpray.SetActive(true);
+            }
+        }
+        else
+        {
+            Vector3 scale = waterCylinder.transform.localScale;
+            scale.z = 250;
+            waterCylinder.transform.localScale = scale;
+
+            if (endSpray != null)
+                endSpray.SetActive(false);
         }
     }
 }
--- Assets/Scripts/UIScripts/PlorpSelect.cs
+++ Assets/Scripts/UIScripts/PlorpSelect.cs
@@ -1,3 +1,5 @@
+using Unity.Mathematics;
+using Unity.Mathematics.Geometry;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,7 +11,13 @@ public class PlorpSelect : MonoBehaviour
     private InputAction _lookAction;
     // private float xRotation;
     private float _yRotation;
-    private readonly float _rotationSpeed = 100f;
+    private readonly float _rotationSpeed = 300f;
+    private bool _isReady = false;
+    // Used in SmoothDampAngle
+    private float _smoothRotateVelocity = 0f;
+    
+    //
+    private const float LookAtScreenRotation = 180f;
 
     public void Initialize(PlayerInput playerInput)
     {
@@ -30,20 +38,32 @@ public class PlorpSelect : MonoBehaviour
     public void Ready()
     {
         plorpAnimator.SetBool("isReady", true);
+        _isReady = true;
     }
 
     public void Unready()
     {
         plorpAnimator.SetBool("isReady", false);
+        _isReady = false;
     }
   
     private void Update()
     {
-        Vector2 lookValue = _lookAction.ReadValue<Vector2>();
-        _yRotation -= lookValue.x * _rotationSpeed * Time.deltaTime; // horizontal (yaw)
-        // x rotation is at feet, disable unless can make pivot point at center
-        // xRotation -= lookValue.y * rotationSpeed * Time.deltaTime;
-        // xRotation = Mathf.Clamp(xRotation, -10f, 10f);
+        if (_isReady)
+        {
+            // Smoothly move look back towards the front and keep it locked there if ready.
+            _yRotation = Mathf.SmoothDampAngle(_yRotation, LookAtScreenRotation, ref _smoothRotateVelocity, 0.2f);
+        }
+        else
+        {
+            // Allow player to rotate the character model when not ready
+            Vector2 lookValue = _lookAction.ReadValue<Vector2>();
+            _yRotation -= lookValue.x * _rotationSpeed * Time.deltaTime; // horizontal (yaw)
+            // x rotation is at feet, disable unless can make pivot point at center
+            // xRotation -= lookValue.y * rotationSpeed * Time.deltaTime;
+            // xRotation = Mathf.Clamp(xRotation, -10f, 10f);
+        }
+
         modelTransform.localRotation = Quaternion.Euler(0f, _yRotation, 0f);
     }
 }
