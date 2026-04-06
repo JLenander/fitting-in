fe076359b922af79fe7819ad3ec057b6d6df978d
Joshua Lenander <42joshl@gmail.com>
2026-04-05 22:26:42 -0400

    Fix chair too easy to sit back down in

--- Assets/Scripts/Level 1/TriggerSeat.cs
+++ Assets/Scripts/Level 1/TriggerSeat.cs
@@ -17,11 +17,24 @@ public class TriggerSeat : MonoBehaviour
 
     private bool _isSitting;
 
+    private float _sitDownDelayCounter = 0f;
+    [SerializeField] private float SitBackDownDelay = 2f;
+
+    private void Awake()
+    {
+        _sitDownDelayCounter = 0f;
+    }
+
+    private void Update()
+    {
+        if (_sitDownDelayCounter > 0f) _sitDownDelayCounter -= Time.deltaTime;
+    }
+    
     // trigger level start at trigger enter
     private void OnTriggerEnter(Collider other)
     {
         if (playerSitting) return;
-        if (other != null && other.CompareTag("Robot"))
+        if (_sitDownDelayCounter <= 0f && other != null && other.CompareTag("Robot"))
         {
             if (!triggered) novaLevel1Manager.PlayLevelRoutine();
             robotMovement.disable = true;
@@ -35,6 +48,8 @@ public class TriggerSeat : MonoBehaviour
     {
         if (other != null && other.CompareTag("Robot"))
         {
+            // Add a small delay to sitting back down to prevent sitting back down too easily
+            _sitDownDelayCounter = SitBackDownDelay;
             playerSitting = false;
             BoxCollider collider = GetComponent<BoxCollider>();
             collider.enabled = true;
